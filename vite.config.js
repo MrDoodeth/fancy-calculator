034bbf654ec7a9db1dcbbc93d0c15e7509b9b4e5
MrDoodeth <mrduves@yandex.ru>
2026-01-31 20:16:08 +0300

    chore: preparing

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
         host: "0.0.0.0",
         port: 5000
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@use "/src/styles/utils" as *;'
-            }
-        }
     },
     preview: {
         port: 9999
